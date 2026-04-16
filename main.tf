0e558413face68919e535c06ee0e58d572461f33
AhuraMagda <104498979+AhuraMagda@users.noreply.github.com>
2026-04-16 21:33:19 +0200

    moving kv to the end

--- main.tf
+++ main.tf
@@ -19,19 +19,6 @@ terraform {
   }
 }
 
-module "keyvault" {
-  source = "git::https://github.com/pchylak/global_azure_2026_ccoe.git?ref=keyvault/v1.0.0"
-  keyvault_name = "gaazuser42026xyz"
-  network_acls = {
-    default_action             = "Deny"
-    bypass                     = "AzureServices"
-  }
-  resource_group = {
-    location = "northeurope"
-    name     = "rg-user4"
-  }
-}
-
 module "service_plan" {
   source = "git::https://github.com/pchylak/global_azure_2026_ccoe.git?ref=service_plan/v2.0.0"
   app_service_plan_name = "agspuser4"
@@ -54,15 +41,28 @@ module "managed_identity" {
   }
 }
 
-# module "app_service" {
-#   source = "git::https://github.com/pchylak/global_azure_2026_ccoe.git?ref=app_service/v1.0.0"
-#   app_service_name = "agappserviceuser4"
-#   app_service_plan_id = module.service_plan.app_service_plan.id
-#   app_settings = {}
-#   identity_client_id = module.managed_identity.managed_identity_client_id
-#   identity_id = module.managed_identity.managed_identity_id
-#   resource_group = {
-#     location = "northeurope"
-#     name     = "rg-user4"
-#   }
-# }
+module "app_service" {
+  source = "git::https://github.com/pchylak/global_azure_2026_ccoe.git?ref=app_service/v1.0.0"
+  app_service_name = "agappserviceuser4"
+  app_service_plan_id = module.service_plan.app_service_plan.id
+  app_settings = {}
+  identity_client_id = module.managed_identity.managed_identity_client_id
+  identity_id = module.managed_identity.managed_identity_id
+  resource_group = {
+    location = "northeurope"
+    name     = "rg-user4"
+  }
+}
+
+module "keyvault" {
+  source = "git::https://github.com/pchylak/global_azure_2026_ccoe.git?ref=keyvault/v1.0.0"
+  keyvault_name = "gaazuser42026xyz"
+  network_acls = {
+    default_action = "Deny"
+    bypass = "AzureServices"
+  }
+  resource_group = {
+    location = "northeurope"
+    name = "rg-user4"
+  }
+}
