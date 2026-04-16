035e27725e80cb12c5e543ac4be7c1e763ecd2fb
AhuraMagda <104498979+AhuraMagda@users.noreply.github.com>
2026-04-16 20:41:18 +0200

    create service plan

--- outputs.tf
+++
@@ -1,8 +0,0 @@
-output "app_service_plan" {
-  value = {
-    name                = module.service_plan.name
-    id                  = module.service_plan.id
-    resource_group_name = module.service_plan.resource_group_name
-  }
-  description = "required output for other modules."
-}
